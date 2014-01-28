dff851527e7e4b09c5223dabf91af9a69b5062f8
programthis <kylebachan@gmail.com>
2014-01-28 13:17:33 -0500

    Pushing base files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photogur2::Application.config.secret_key_base = 'e9ae80f5b49f2f5ecee54dcf9860a4fd6ba2f170a4355b2a465d57d42ecaf051beb82d80c491b176f27dd44f70784888e1cd34ea15671b3f24a9756f4894454a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Photogur2::Application.config.session_store :cookie_store, key: '_photogur2_session'
